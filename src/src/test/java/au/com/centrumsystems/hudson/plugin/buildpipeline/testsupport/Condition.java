1cc5961f1fc08bd63b4168d464ef541c1ae7e3d8
Jiapeng Guo <b52813@freescale.com>
2014-11-28 13:21:21 +0800

    Delete unuseful operation

--- src/src/test/java/au/com/centrumsystems/hudson/plugin/buildpipeline/testsupport/Condition.java
+++
@@ -1,7 +0,0 @@
-package au.com.centrumsystems.hudson.plugin.buildpipeline.testsupport;
-
-public interface Condition {
-
-    boolean isSatisfied() throws Exception;
-    String describe();
-}
--- src/src/test/java/au/com/centrumsystems/hudson/plugin/buildpipeline/testsupport/Page.java
+++
@@ -1,8 +0,0 @@
-package au.com.centrumsystems.hudson.plugin.buildpipeline.testsupport;
-
-import java.net.URI;
-
-public interface Page {
-
-    String getRelativeUrl();
-}
